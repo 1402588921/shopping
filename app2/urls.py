2702235702bf61fba178298e3ad129ac5f30c7eb
岁月年华 <1402588921@qq.com>
2024-09-26 23:13:19 +0800

    learn HttpResponse and HttpRequest class

--- app2/urls.py
+++ app2/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import re_path, path
 from app2 import views
 
 
 urlpatterns = [
-    path(r'app2/show/(?P<year>\d+)/', views.index)
+    re_path(r'app2/show/(?P<year>\d{4})/', views.index),
+    re_path(r'app2/show/(?P<page>\d+)&key=(?P<key>\w+)', views.article_page),
+    path(
+        'app2/url_reverse/<int:id>', views.url_reverse, name='app2_url_reverse'
+    ),
+    path('app2/test_post/', views.test_post),
+    path('app2/test_response/', views.test_response),
+    path('app2/test_render/', views.test_render),
 ]
--- app2/views.py
+++ app2/views.py
@@ -1,5 +1,54 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from models import UserBaseInfo
 
 
-def index(request, year):
+def index(request: HttpRequest, year):
+    print(request.GET.get('username'))
     return HttpResponse(f'app2 中的 show 方法，参数的值为{year}')
+
+
+def article_page(request: HttpRequest, page: int, key: str):
+    return HttpResponse(f'传入的页数是:{page}，传入的key={key}')
+
+
+def url_reverse(request: HttpRequest, id: int):
+    return render(request, '2/url_reverse.html')
+
+
+def test_post(request: HttpRequest):
+    print(request.method)
+    print(request.POST.get('username'))
+    return render(request, '2/test_post.html')
+
+
+def test_response(request: HttpRequest):
+    response = HttpResponse()
+    response.write('Hello Django')
+    response.write('<br>')
+    response.write(response.content)
+    response.write('<br>')
+    response.write(response['content-type'])
+    response.write('<br>')
+    response.write(response.charset)
+    response.write(response.status_code)
+    response.write('<br>')
+    return response
+
+
+def test_render(request: HttpRequest):
+    return render(
+        request,
+        '2/test_render.html',
+        dict(info='Hello Django'),
+        content_type='text/html'
+    )
+
+
+def test_redirect_model(request: HttpRequest, id: int):
+    user = UserBaseInfo.objects.get(id=id)
+
+
+def userinfo(request: HttpRequest, id: int):
+    user = UserBaseInfo.objects.get(id=id)
+    return HttpResponse('编号' + str(user.id) + '姓名' + user.username)
